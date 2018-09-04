8094d8767993a436bbf7479b7fed7aacc623f5dd
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAs-MacBook-Pro.local>
2018-09-04 08:57:31 -0400

    depth first search inorder

--- treeTraversal.js
+++ treeTraversal.js
@@ -1,5 +1,5 @@
 let { BST, Node } = require("./binarySearchTree.js");
-let { Queue } = require("./queue.js");
+// let { Queue } = require("./queue.js");
 
 
 let bst = new BST();
@@ -36,5 +36,45 @@ function breadthFirstSearchTraversal(root) {
 }
 
 let result = breadthFirstSearchTraversal(bst.root);
-console.log(result);
+// console.log(result);
 // [10, 5, 15, 1, 6, 12, 20];
+
+// if you have a binary search tree and you do depthFirstSearchInorder you get a sorted array
+function depthFirstSearchInorder(root){
+	// left root right
+	let storage = [];
+	function recursiveInorder(root){
+		// base case
+		// found a leaf node
+		if(!root.left && !root.right){
+			storage.push(root.value);
+			return;
+		}
+		if(root.left){
+			recursiveInorder(root.left);
+		}
+		storage.push(root.value);
+		if(root.right){
+			recursiveInorder(root.right);
+		}
+	}
+	recursiveInorder(root);
+	return storage;
+}
+
+let bst00 = new BST();
+bst00.insert(20);
+bst00.insert(15);
+bst00.insert(10);
+bst00.insert(8);
+bst00.insert(12);
+bst00.insert(25);
+bst00.insert(21);
+bst00.insert(30);
+
+let inorder = depthFirstSearchInorder(bst00.root);
+console.log(bst00);
+console.log(inorder);
+
+
+
