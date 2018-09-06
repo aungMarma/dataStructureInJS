6626d20e1ec531a25ebf7910722e1254ad047afe
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAsMBP.home>
2018-09-05 23:23:49 -0400

    in pre post order traversal

--- treeTraversal.js
+++ treeTraversal.js
@@ -1,7 +1,6 @@
 let { BST, Node } = require("./binarySearchTree.js");
 // let { Queue } = require("./queue.js");
 
-
 let bst = new BST();
 bst.insert(10);
 bst.insert(5);
@@ -11,6 +10,7 @@ bst.insert(20);
 bst.insert(1);
 bst.insert(6);
 
+// level by level, level first before going to depth
 function breadthFirstSearchTraversal(root) {
 	let queue = [];   // push, shift
 	let storage = [];
@@ -39,17 +39,18 @@ let result = breadthFirstSearchTraversal(bst.root);
 // console.log(result);
 // [10, 5, 15, 1, 6, 12, 20];
 
+// depth first, left first before right!
+// left => root => right
 // if you have a binary search tree and you do depthFirstSearchInorder you get a sorted array
 function depthFirstSearchInorder(root){
-	// left root right
 	let storage = [];
 	function recursiveInorder(root){
 		// base case
 		// found a leaf node
-		if(!root.left && !root.right){
-			storage.push(root.value);
-			return;
-		}
+		// if(!root.left && !root.right){
+		// 	storage.push(root.value);
+		// 	return;
+		// }
 		if(root.left){
 			recursiveInorder(root.left);
 		}
@@ -73,8 +74,65 @@ bst00.insert(21);
 bst00.insert(30);
 
 let inorder = depthFirstSearchInorder(bst00.root);
-console.log(bst00);
 console.log(inorder);
+// [ 8, 10, 12, 15, 20, 21, 25, 30 ]
 
+
+// root => left => right
+function depthFirstSearchPreorder(root){
+	let storage = [];
+	function recursivePreorder(root){
+		storage.push(root.value);
+		if(root.left){
+			recursivePreorder(root.left);
+		}
+		if(root.right){
+			recursivePreorder(root.right);
+		}
+	}
+	recursivePreorder(root);
+	return storage;
+}
+
+let bst01 = new BST();
+bst01.insert(15);
+bst01.insert(20);
+bst01.insert(10);
+bst01.insert(8);
+bst01.insert(12);
+bst01.insert(17);
+bst01.insert(25);
+/*
+						15
+		10							20
+8				12			17				25
+*/
+let preorder = depthFirstSearchPreorder(bst01.root);
+console.log(preorder);
+// [ 15, 10, 8, 12, 20, 17, 25 ]
+
+// left => right => root
+function depthFirstSearchPostorder(root){
+	let storage = [];
+	function recursivePostorder(root){
+		if(root.left){
+			recursivePostorder(root.left);
+		}
+		if(root.right){
+			recursivePostorder(root.right);
+		}
+		storage.push(root.value);
+	}
+	recursivePostorder(root);
+	return storage;
+}
+/*
+						15
+		10							20
+8				12			17				25
+*/
+let postorder = depthFirstSearchPostorder(bst01.root);
+console.log(postorder);
+// [ 8, 12, 10, 17, 25, 20, 15 ]
 
 
